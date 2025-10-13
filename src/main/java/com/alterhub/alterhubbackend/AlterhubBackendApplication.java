e82a2eabcb564152f286ee3f3cceca2cf65197cf
Jonathan <jonathan.schmidt.13@gmail.com>
2025-10-13 12:33:12 +0200

    initialisation du projet

---
+++ src/main/java/com/alterhub/alterhubbackend/AlterhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alterhub.alterhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlterhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlterhubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alterhub/alterhubbackend/AlterhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alterhub.alterhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlterhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

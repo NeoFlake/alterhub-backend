0689f6f7593f0b418eb7f414028e6c53b9541fde
Jonathan <jonathan.schmidt.13@gmail.com>
2025-10-28 16:28:40 +0100

    Création service role fini, v1

---
+++ src/main/java/com/alterhub/alterhubbackend/service/impl/RoleServiceImpl.java
@@ -0,0 +1,50 @@
+package com.alterhub.alterhubbackend.service.impl;
+
+import com.alterhub.alterhubbackend.dto.RoleDTO;
+import com.alterhub.alterhubbackend.entity.Role;
+import com.alterhub.alterhubbackend.exception.IdNotMatchException;
+import com.alterhub.alterhubbackend.exception.NoResultByIdException;
+import com.alterhub.alterhubbackend.mapper.RoleMapper;
+import com.alterhub.alterhubbackend.repository.RoleRepository;
+import com.alterhub.alterhubbackend.service.interfaces.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public Boolean permissionGrantedForUser(UUID id) {
+        return roleRepository.existsByUserIdAndRole(id, com.alterhub.alterhubbackend.enums.Role.ADMIN);
+    }
+
+    public RoleDTO addRole(RoleDTO roleDTO) {
+        return RoleMapper.toDto(roleRepository.save(RoleMapper.toEntity(roleDTO)));
+    }
+
+    public RoleDTO updateRoleById(UUID id, RoleDTO roleDTO) {
+        if(roleDTO.getId().equals(id)) {
+            Role roleToUpdate = roleRepository.findById(id).orElseThrow(NoResultByIdException::new);
+            Role roleUpdated = RoleMapper.toEntity(roleDTO);
+
+            roleToUpdate.setUserId(roleUpdated.getUserId());
+            roleToUpdate.setRole(roleUpdated.getRole());
+            return RoleMapper.toDto(roleRepository.save(roleToUpdate));
+
+        } else {
+            throw new IdNotMatchException();
+        }
+    }
+
+    public void deleteRoleById(UUID id) {
+        if(!roleRepository.existsById(id)) {
+            throw new NoResultByIdException();
+        }
+        roleRepository.deleteById(id);
+    }
+
+}
---
+++ src/main/java/com/alterhub/alterhubbackend/service/interfaces/RoleService.java
@@ -0,0 +1,17 @@
+package com.alterhub.alterhubbackend.service.interfaces;
+
+import com.alterhub.alterhubbackend.dto.RoleDTO;
+
+import java.util.UUID;
+
+public interface RoleService {
+
+    RoleDTO addRole(RoleDTO roleDTO);
+
+    RoleDTO updateRoleById(UUID id, RoleDTO roleDTO);
+
+    void deleteRoleById(UUID id);
+
+    Boolean permissionGrantedForUser(UUID id);
+
+}

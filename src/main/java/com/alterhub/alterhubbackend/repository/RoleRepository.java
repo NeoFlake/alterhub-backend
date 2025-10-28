f0c4a197c86dd340608a96bcc18cfbc3bf7f0cff
Jonathan <jonathan.schmidt.13@gmail.com>
2025-10-28 16:29:41 +0100

    Création service role fini, v1.2

--- src/main/java/com/alterhub/alterhubbackend/repository/RoleRepository.java
+++ src/main/java/com/alterhub/alterhubbackend/repository/RoleRepository.java
@@ -3,12 +3,9 @@ package com.alterhub.alterhubbackend.repository;
 import com.alterhub.alterhubbackend.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
-
-    Optional<Role> findByUserId(UUID userId);
 
     Boolean existsByUserIdAndRole(UUID userId, com.alterhub.alterhubbackend.enums.Role role);
 }
